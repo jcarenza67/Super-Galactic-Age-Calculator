04eaf72e773e02fd77a2ea0b043c112a5afa0e9f
Joseph Wilfong <josephwilfong91@gmail.com>
2023-04-16 14:27:08 -0700

    create test for future Earth

--- __tests__/Super.test.js
+++ __tests__/Super.test.js
@@ -83,17 +83,15 @@ describe('SpaceAge', () => {
     expect(yearsPassed.jupiterYears).toBeGreaterThanOrEqual(1);
   });
 
-  //   test('should return the correct years left until the users future birthday on Earth', () => {
-  //     const userBirthday = new Date('1991-09-28');
-  //     const currentDate = new Date();
-  //     const milliseconds = currentDate - userBirthday;
-  //     const age = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
+  test('should return the correct years left until the users future birthday on Earth', () => {
+    const userBirthday = new Date('1991-09-28');
+    const age = 31;
 
-  //     const spaceAge = new SpaceAge(age);
-  //     const futureBirthday = new Date('2041-09-28');
-  //     const yearsLeft = spaceAge.yearsLeft(futureBirthday);
-  //     expect(yearsLeft.earthYears).toBeGreaterThanOrEqual(18);
-  //   });
+    const spaceAge = new SpaceAge(age, userBirthday);
+    const futureBirthday = new Date('2041-09-28');
+    const yearsLeft = spaceAge.yearsLeft(futureBirthday);
+    expect(yearsLeft.earthYears).toBeGreaterThanOrEqual(18);
+  });
 
   //   test('should return the correct years left until the users future birthday on Mercury', () => {
   //     const userBirthday = new Date('1991-09-28');
