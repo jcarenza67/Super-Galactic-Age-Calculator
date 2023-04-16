ae5a71980e6e9bda8a7fae2ea3cc5bd0f8fa56f9
Joseph Wilfong <josephwilfong91@gmail.com>
2023-04-16 14:04:43 -0700

    Starting over again, over complicated it, and created a test for Earth passed birthdate based on users birthday and not current date

--- __tests__/Super.test.js
+++ __tests__/Super.test.js
@@ -35,85 +35,83 @@ describe('SpaceAge', () => {
 
   test('should return the correct years passed on Earth since a past birthday based on users bday', () => {
     const userBirthday = new Date('1991-09-28');
-    const currentDate = new Date();
-    const milliseconds = currentDate - userBirthday;
-    const age = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
+    const age = 31;
 
-    const spaceAge = new SpaceAge(age);
+    const spaceAge = new SpaceAge(age, userBirthday);
     const pastBirthday = new Date('2000-09-28');
     const yearsPassed = spaceAge.yearsPassed(pastBirthday);
-    expect(yearsPassed.earthYears).toBeGreaterThanOrEqual(8);
+    expect(yearsPassed.earthYears).toBeGreaterThanOrEqual(10);
   });
 
-  test('should return the correct years passed on Mercury since a past birthday based on users bday', () => {
-    const userBirthday = new Date('1991-09-28');
-    const currentDate = new Date();
-    const milliseconds = currentDate - userBirthday;
-    const age = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
+  //   test('should return the correct years passed on Mercury since a past birthday based on users bday', () => {
+  //     const userBirthday = new Date('1991-09-28');
+  //     const currentDate = new Date();
+  //     const milliseconds = currentDate - userBirthday;
+  //     const age = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
 
-    const spaceAge = new SpaceAge(age);
-    const pastBirthday = new Date('2000-09-28');
-    const yearsPassed = spaceAge.yearsPassed(pastBirthday);
-    expect(yearsPassed.mercuryYears).toBeGreaterThanOrEqual(33);
-  });
+  //     const spaceAge = new SpaceAge(age);
+  //     const pastBirthday = new Date('2000-09-28');
+  //     const yearsPassed = spaceAge.yearsPassed(pastBirthday);
+  //     expect(yearsPassed.mercuryYears).toBeGreaterThanOrEqual(33);
+  //   });
 
-  test('should return the correct years passed on Venus since a past birthday based on users bday', () => {
-    const userBirthday = new Date('1991-09-28');
-    const currentDate = new Date();
-    const milliseconds = currentDate - userBirthday;
-    const age = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
+  //   test('should return the correct years passed on Venus since a past birthday based on users bday', () => {
+  //     const userBirthday = new Date('1991-09-28');
+  //     const currentDate = new Date();
+  //     const milliseconds = currentDate - userBirthday;
+  //     const age = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
 
-    const spaceAge = new SpaceAge(age);
-    const pastBirthday = new Date('2000-09-28');
-    const yearsPassed = spaceAge.yearsPassed(pastBirthday);
-    expect(yearsPassed.venusYears).toBeGreaterThanOrEqual(13);
-  });
+  //     const spaceAge = new SpaceAge(age);
+  //     const pastBirthday = new Date('2000-09-28');
+  //     const yearsPassed = spaceAge.yearsPassed(pastBirthday);
+  //     expect(yearsPassed.venusYears).toBeGreaterThanOrEqual(13);
+  //   });
 
-  test('should return the correct years passed on Mars since a past birthday based on users bday', () => {
-    const userBirthday = new Date('1991-09-28');
-    const currentDate = new Date();
-    const milliseconds = currentDate - userBirthday;
-    const age = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
+  //   test('should return the correct years passed on Mars since a past birthday based on users bday', () => {
+  //     const userBirthday = new Date('1991-09-28');
+  //     const currentDate = new Date();
+  //     const milliseconds = currentDate - userBirthday;
+  //     const age = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
 
-    const spaceAge = new SpaceAge(age);
-    const pastBirthday = new Date('2000-09-28');
-    const yearsPassed = spaceAge.yearsPassed(pastBirthday);
-    expect(yearsPassed.marsYears).toBeGreaterThanOrEqual(4);
-  });
+  //     const spaceAge = new SpaceAge(age);
+  //     const pastBirthday = new Date('2000-09-28');
+  //     const yearsPassed = spaceAge.yearsPassed(pastBirthday);
+  //     expect(yearsPassed.marsYears).toBeGreaterThanOrEqual(4);
+  //   });
 
-  test('should return the correct years passed on Jupiter since a past birthday based on users bday', () => {
-    const userBirthday = new Date('1991-09-28');
-    const currentDate = new Date();
-    const milliseconds = currentDate - userBirthday;
-    const age = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
+  //   test('should return the correct years passed on Jupiter since a past birthday based on users bday', () => {
+  //     const userBirthday = new Date('1991-09-28');
+  //     const currentDate = new Date();
+  //     const milliseconds = currentDate - userBirthday;
+  //     const age = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
 
-    const spaceAge = new SpaceAge(age);
-    const pastBirthday = new Date('2000-09-28');
-    const yearsPassed = spaceAge.yearsPassed(pastBirthday);
-    expect(yearsPassed.jupiterYears).toBeGreaterThanOrEqual(1);
-  });
+  //     const spaceAge = new SpaceAge(age);
+  //     const pastBirthday = new Date('2000-09-28');
+  //     const yearsPassed = spaceAge.yearsPassed(pastBirthday);
+  //     expect(yearsPassed.jupiterYears).toBeGreaterThanOrEqual(1);
+  //   });
 
-  test('should return the correct years left until the users future birthday on Earth', () => {
-    const userBirthday = new Date('1991-09-28');
-    const currentDate = new Date();
-    const milliseconds = currentDate - userBirthday;
-    const age = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
+  //   test('should return the correct years left until the users future birthday on Earth', () => {
+  //     const userBirthday = new Date('1991-09-28');
+  //     const currentDate = new Date();
+  //     const milliseconds = currentDate - userBirthday;
+  //     const age = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
 
-    const spaceAge = new SpaceAge(age);
-    const futureBirthday = new Date('2041-09-28');
-    const yearsLeft = spaceAge.yearsLeft(futureBirthday);
-    expect(yearsLeft.earthYears).toBeGreaterThanOrEqual(18);
-  });
+  //     const spaceAge = new SpaceAge(age);
+  //     const futureBirthday = new Date('2041-09-28');
+  //     const yearsLeft = spaceAge.yearsLeft(futureBirthday);
+  //     expect(yearsLeft.earthYears).toBeGreaterThanOrEqual(18);
+  //   });
 
-  test('should return the correct years left until the users future birthday on Mercury', () => {
-    const userBirthday = new Date('1991-09-28');
-    const currentDate = new Date();
-    const milliseconds = currentDate - userBirthday;
-    const age = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
+  //   test('should return the correct years left until the users future birthday on Mercury', () => {
+  //     const userBirthday = new Date('1991-09-28');
+  //     const currentDate = new Date();
+  //     const milliseconds = currentDate - userBirthday;
+  //     const age = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
 
-    const spaceAge = new SpaceAge(age);
-    const futureBirthday = new Date('2041-09-28');
-    const yearsLeft = spaceAge.yearsLeft(futureBirthday);
-    expect(yearsLeft.mercuryYears).toBeGreaterThanOrEqual(75);
-  });
+  //     const spaceAge = new SpaceAge(age);
+  //     const futureBirthday = new Date('2041-09-28');
+  //     const yearsLeft = spaceAge.yearsLeft(futureBirthday);
+  //     expect(yearsLeft.mercuryYears).toBeGreaterThanOrEqual(75);
+  //   });
 });
--- src/Super-Galactic.js
+++ src/Super-Galactic.js
@@ -23,26 +23,4 @@ export class SpaceAge{
     return Math.round(this.age / 11.86);
   }
 
-  yearsPassed(pastBirthday) {
-    const currentDate = new Date();
-    const milliseconds =  currentDate - pastBirthday;
-    const earthYears = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
-
-    return {earthYears,
-      mercuryYears: Math.round(earthYears / .24),
-      venusYears: Math.round(earthYears / .62),
-      marsYears: Math.round(earthYears / 1.88),
-      jupiterYears: Math.round(earthYears / 11.86)
-    };
-  }          
-
-  yearsLeft(futureBirthday) {
-    const currentDate = new Date();
-    const milliseconds =  futureBirthday - currentDate;
-    const earthYears = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
-
-    return {earthYears,
-      mercuryYears: Math.round(earthYears / .24),
-    };
-  }
 }
