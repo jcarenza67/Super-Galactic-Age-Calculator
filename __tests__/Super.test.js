33c0e143c5861ec5582cad112b86182c8c6ee3e3
Joseph Wilfong <josephwilfong91@gmail.com>
2023-04-16 12:48:14 -0700

    refactor test again, realized some errors then will test

--- __tests__/Super.test.js
+++ __tests__/Super.test.js
@@ -34,55 +34,59 @@ describe('SpaceAge', () => {
   });
 
   test('should return the correct years passed on Earth since a past birthday based on users bday', () => {
-    const userBirthday = new Date('1991-28-09');
-    const spaceAge = new Date(userBirthday);
-    const pastBirthday = new Date('2000-28-09');
-    const result = spaceAge.yearsPassed(pastBirthday);
-    expect(result.earthYears).toBeGreaterThanOrEqualTo(8);
+    const userBirthday = new Date(1991-09-28);
+    const currentDate = new Date();
+    const milliseconds = currentDate - userBirthday;
+    const age = Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 365.25));
+
+    const spaceAge = new SpaceAge(age);
+    const pastBirthday = new Date(2000-09-28);
+    const yearsPassed = spaceAge.yearsPassed(pastBirthday);
+    expect(yearsPassed.earthYears).toBeGreaterThanOrEqualTo(8);
   });
 
-  test('should return the correct years passed on Mercury since a past birthday', () => {
-    const result = spaceAge.yearsPassed(20);
-    expect(result.mercuryYears).toEqual(42);
-  });
+  // test('should return the correct years passed on Mercury since a past birthday', () => {
+  //   const result = spaceAge.yearsPassed(20);
+  //   expect(result.mercuryYears).toEqual(42);
+  // });
 
-  test('should return the correct years passed on Venus since a past birthday', () => {
-    const result = spaceAge.yearsPassed(20);
-    expect(result.venusYears).toEqual(16);
-  });
+  // test('should return the correct years passed on Venus since a past birthday', () => {
+  //   const result = spaceAge.yearsPassed(20);
+  //   expect(result.venusYears).toEqual(16);
+  // });
 
-  test('should return the correct years passed on Mars since a past birthday', () => {
-    const result = spaceAge.yearsPassed(20);
-    expect(result.marsYears).toEqual(5);
-  });
+  // test('should return the correct years passed on Mars since a past birthday', () => {
+  //   const result = spaceAge.yearsPassed(20);
+  //   expect(result.marsYears).toEqual(5);
+  // });
 
-  test('should return the correct years passed on Jupiter since a past birthday', () => {
-    const result = spaceAge.yearsPassed(20);
-    expect(result.jupiterYears).toEqual(1);
-  });
+  // test('should return the correct years passed on Jupiter since a past birthday', () => {
+  //   const result = spaceAge.yearsPassed(20);
+  //   expect(result.jupiterYears).toEqual(1);
+  // });
 
-  test('should return the correct years left until next birthday on Earth', () => {
-    const result = spaceAge.yearsLeft(100);
-    expect(result.earthYears).toEqual(70);
-  });
+  // test('should return the correct years left until next birthday on Earth', () => {
+  //   const result = spaceAge.yearsLeft(100);
+  //   expect(result.earthYears).toEqual(70);
+  // });
 
-  test('should return the correct years left until next birthday on Mercury', () => {
-    const result = spaceAge.yearsLeft(100);
-    expect(result.mercuryYears).toEqual(292);
-  });
+  // test('should return the correct years left until next birthday on Mercury', () => {
+  //   const result = spaceAge.yearsLeft(100);
+  //   expect(result.mercuryYears).toEqual(292);
+  // });
 
-  test('should return the correct years left until next birthday on Venus', () => {
-    const result = spaceAge.yearsLeft(100);
-    expect(result.venusYears).toEqual(113);
-  });
+  // test('should return the correct years left until next birthday on Venus', () => {
+  //   const result = spaceAge.yearsLeft(100);
+  //   expect(result.venusYears).toEqual(113);
+  // });
 
-  test('should return the correct years left until next birthday on Mars', () => {
-    const result = spaceAge.yearsLeft(100);
-    expect(result.marsYears).toEqual(37);
-  });
+  // test('should return the correct years left until next birthday on Mars', () => {
+  //   const result = spaceAge.yearsLeft(100);
+  //   expect(result.marsYears).toEqual(37);
+  // });
 
-  test('should return the correct years left until next birthday on Jupiter', () => {
-    const result = spaceAge.yearsLeft(100);
-    expect(result.jupiterYears).toEqual(6);
-  });
+  // test('should return the correct years left until next birthday on Jupiter', () => {
+  //   const result = spaceAge.yearsLeft(100);
+  //   expect(result.jupiterYears).toEqual(6);
+  // });
 });
