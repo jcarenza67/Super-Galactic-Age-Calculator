4a116f3d1b4aa45af471e0b6e99c6db697edddb8
Joseph Wilfong <josephwilfong91@gmail.com>
2023-04-16 17:35:58 -0700

    pass test Mercury past decimal

--- src/Super-Galactic.js
+++ src/Super-Galactic.js
@@ -28,7 +28,7 @@ export class SpaceAge{
   yearsPassed(pastBirthday) {
     const earthYears = this.age - pastBirthday;
     return {earthYears,
-      mercuryYears: Math.round(earthYears / .24),
+      mercuryYears: parseFloat((earthYears / .24).toFixed(2)),
       venusYears: Math.round(earthYears / .62),
       marsYears: Math.round(earthYears / 1.88),
       jupiterYears: Math.round(earthYears / 11.86),
